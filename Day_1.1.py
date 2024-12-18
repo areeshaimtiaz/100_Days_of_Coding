90425c100769591956c746e5782b4fb1310f9b4c
Areesha Imtiaz <76179142+areeshaimtiaz@users.noreply.github.com>
2024-12-18 15:06:41 +0400

    Add files via upload

---
+++ Day_1.1.py
@@ -0,0 +1,13 @@
+# Printing Practice
+print("Hello, World!\n") # \n creates a white line
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.\n")
+
+print("Hello, World!\nHello, World!\n")
+
+# String Concatenation
+print("Hello " + "Areesha")
