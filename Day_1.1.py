92e769166281010e54e5e482789f5046710479a8
Areesha Imtiaz <76179142+areeshaimtiaz@users.noreply.github.com>
2024-12-18 15:42:49 +0400

    Add files via upload

--- Day_1.1.py
+++ Day_1.1.py
@@ -10,4 +10,30 @@ print("5. Bake at 200 degrees C for 30 minutes.\n")
 print("Hello, World!\nHello, World!\n")
 
 # String Concatenation
-print("Hello " + "Areesha")
+print("Hello " + "Areesha\n")
+
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n\n")
+
+# Input
+# Inside the parenthesis is a prompt given to the user
+name = input("Enter your name: ")
+print("Hello, " + name + "!\n")
+
+# String Length
+nationality = input("Enter your nationality: ")
+print(len(nationality))
+
+username = input("Enter your username: ")
+length = len(username)
+print(length)
+
+# Switching the value of 2 variables
+glass1 = "milk"
+glass2 = "juice"
+temp = "juice"
+glass2 = glass1
+glass1 = temp
